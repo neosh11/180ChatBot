79b5f96ad073e56781171e041d96b6657173f4c2
neosh <neosh11@gmail.com>
2017-12-14 17:15:23 +0800

    Removed port stuff

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
+/*
+  NY -Not yet used
+*/
+
+
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var path = require('path'); //NY
+var favicon = require('serve-favicon'); //NY
+var logger = require('morgan'); //NY
+var cookieParser = require('cookie-parser'); 
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('.server/routes/index');
+var users = require('.server/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/server/views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
