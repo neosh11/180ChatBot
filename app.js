bd2eec0f54873e07ea0035e8f4196de461adcd18
neosh <neosh11@gmail.com>
2017-12-14 22:02:19 +0800

    Added response functionality

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
   NY -Not yet used
 */
 
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 var express = require('express');
 var path = require('path'); //NY
--- server/routes/index.js
+++ server/routes/index.js
@@ -114,7 +114,7 @@ function handlePostback(sender_psid, received_postback) {
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
