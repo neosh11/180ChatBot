bd1d19634ae8031cea0a03f3859ceceb722b9a57
neosh <neosh11@gmail.com>
2018-01-05 16:30:30 +0800

    update

--- server/routes/index.js
+++ server/routes/index.js
@@ -85,49 +85,97 @@ router.get('/webhook', (req, res) => {
 
 function handleMessage(sender_psid, received_message) {
   
-    let response;
-  
-    // Check if the message contains text
-    if (received_message.text) {    
-  
-      // Create the payload for a basic text message
-      response = {
-        "text": `You sent the message: "${received_message.text}". Now send me an image!`
-      }
-    }  
-    
-    // Sends the response message
-    callSendAPI(sender_psid, response);    
+  let response;
+
+  // Check if the message contains text
+
+
+  if(received_message.quick_reply.payload)
+  {
+    switch(received_message.quick_reply.payload)
+    {
+      case "F": 
+      callFeedback(sender_psid)
+      break;
+
+      case "S":
+
+      break;
+
+      default: break;
+    }
   }
 
+  else if (received_message.text) {    
 
-  // Handles messaging_postbacks events
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+    // Sends the response message
+    callSendAPI(sender_psid, response); 
+  }
+
+}
+
+// Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
   let response;
   
-    // Check if the message contains text
-    if (received_postback.payload == "get started")
-    {
-      response = {
-        "text": `What would you like me to do?`,
-        "quick_replies":[
-          {
-            "content_type":"text",
-            "title":"Feedback",
-            "payload":"F"
-          },
-          {
-            "content_type":"text",
-            "title":"Survey",
-            "payload":"S"
-          }
-        ]
-      }
-    } 
+  // Check if the message contains text
+  if (received_postback.payload == "get started")
+  {
+    response = {
+      "text": `What would you like me to do?`,
+      "quick_replies":[
+        {
+          "content_type":"text",
+          "title":"Feedback",
+          "payload":"F"
+        },
+        {
+          "content_type":"text",
+          "title":"Survey",
+          "payload":"S"
+        }
+      ]
+    }
+  } 
 
-    callQuickSendAPI(sender_psid, response);
+  callQuickSendAPI(sender_psid, response);
 }
  
+
+function callFeedback(sender_psid, received_postback)
+{
+  response = {
+    "text": `Type me some feedback fam!`,
+    "payload":"F"
+  }
+
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+
+}
+
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
   // Construct the message body
@@ -176,8 +224,5 @@ function callQuickSendAPI(sender_psid, response) {
       }
     }); 
 }
-
-
-
 
 module.exports = router;
